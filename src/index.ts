9f6db49fe92f10823c0ea526aa6b75b97ceb63d2
Emerson Paiva <emersonaraujopaiva@gmail.com>
2022-04-25 22:24:04 -0300

    feat(User): add User class to app

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'My name', age: 30 });
 
-console.log(user.get('name'));
+user.on('change', () => {
+  console.log('change');
+});
 
-user.set({ name: 'Pedrinho', age: 26 });
-
-console.log(user.get('name'));
+user.trigger('change');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,10 @@
 type UserProps = { name?: string; age?: number };
 
+type Callback = () => void;
+
 export class User {
+  private events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
@@ -9,5 +13,17 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] ?? [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    handlers?.forEach((callback) => callback());
   }
 }
